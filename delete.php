27611a307222ddc06262bebde0f59266979e2428
MarDev <rozakadm@gmail.com>
2024-06-28 13:29:28 +0700

    Update

--- delete.php
+++ delete.php
@@ -3,8 +3,9 @@ require 'db.php';
 
 $id = $_GET['id'];
 $sql = "DELETE FROM students WHERE id = ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$id]);
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
 
 header("Location: index.php");
 exit;
